b288c50fe963d6389e3b85320a2c8c54783de439
ytower_Topeka <yuri_f@id.uff.br>
2023-07-18 20:16:37 -0300

    feat: add all archives

---
+++ map-editing/obstacles.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="obstacles" tilewidth="148" tileheight="160" tilecount="5" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0" type="Tree">
+  <image width="64" height="64" source="../assets/map/tree.png"/>
+ </tile>
+ <tile id="1" type="Misc">
+  <image width="32" height="32" source="../assets/map/sign.png"/>
+ </tile>
+ <tile id="2" type="Grass">
+  <image width="28" height="28" source="../assets/map/grass.png"/>
+ </tile>
+ <tile id="3" type="Building">
+  <image width="148" height="160" source="../assets/map/house.png"/>
+ </tile>
+ <tile id="4" type="Invisible">
+  <image width="32" height="32" source="../assets/invisible-sprite.png"/>
+ </tile>
+</tileset>
---
+++ map-editing/world.tsx
@@ -0,0 +1,70 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="world" tilewidth="32" tileheight="32" tilecount="1440" columns="40">
+ <image source="../assets/map/world.png" width="1280" height="1152"/>
+ <tile id="1177" probability="0.3"/>
+ <wangsets>
+  <wangset name="Paths" type="corner" tile="-1">
+   <wangcolor name="grass" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="path" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="120" wangid="0,1,0,2,0,1,0,1"/>
+   <wangtile tileid="121" wangid="0,1,0,2,0,2,0,1"/>
+   <wangtile tileid="122" wangid="0,1,0,1,0,2,0,1"/>
+   <wangtile tileid="160" wangid="0,2,0,2,0,1,0,1"/>
+   <wangtile tileid="161" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="162" wangid="0,1,0,1,0,2,0,2"/>
+   <wangtile tileid="200" wangid="0,2,0,1,0,1,0,1"/>
+   <wangtile tileid="201" wangid="0,2,0,1,0,1,0,2"/>
+   <wangtile tileid="202" wangid="0,1,0,1,0,1,0,2"/>
+   <wangtile tileid="240" wangid="0,2,0,1,0,2,0,2"/>
+   <wangtile tileid="241" wangid="0,2,0,2,0,1,0,2"/>
+   <wangtile tileid="280" wangid="0,1,0,2,0,2,0,2"/>
+   <wangtile tileid="281" wangid="0,2,0,2,0,2,0,1"/>
+  </wangset>
+  <wangset name="Dark grass patches" type="corner" tile="-1">
+   <wangcolor name="grass" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="dark grass" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="1174" wangid="0,1,0,2,0,1,0,1"/>
+   <wangtile tileid="1175" wangid="0,1,0,2,0,2,0,1"/>
+   <wangtile tileid="1176" wangid="0,1,0,1,0,2,0,1"/>
+   <wangtile tileid="1177" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="1214" wangid="0,2,0,2,0,1,0,1"/>
+   <wangtile tileid="1215" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="1216" wangid="0,1,0,1,0,2,0,2"/>
+   <wangtile tileid="1254" wangid="0,2,0,1,0,1,0,1"/>
+   <wangtile tileid="1255" wangid="0,2,0,1,0,1,0,2"/>
+   <wangtile tileid="1256" wangid="0,1,0,1,0,1,0,2"/>
+   <wangtile tileid="1294" wangid="0,2,0,1,0,2,0,2"/>
+   <wangtile tileid="1295" wangid="0,2,0,2,0,1,0,2"/>
+   <wangtile tileid="1334" wangid="0,1,0,2,0,2,0,2"/>
+   <wangtile tileid="1335" wangid="0,2,0,2,0,2,0,1"/>
+  </wangset>
+  <wangset name="Lake" type="corner" tile="-1">
+   <wangcolor name="grass" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="water" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="242" wangid="0,1,0,2,0,1,0,1"/>
+   <wangtile tileid="243" wangid="0,1,0,2,0,2,0,1"/>
+   <wangtile tileid="244" wangid="0,1,0,1,0,2,0,1"/>
+   <wangtile tileid="282" wangid="0,2,0,2,0,1,0,1"/>
+   <wangtile tileid="283" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="284" wangid="0,1,0,1,0,2,0,2"/>
+   <wangtile tileid="322" wangid="0,2,0,1,0,1,0,1"/>
+   <wangtile tileid="323" wangid="0,2,0,1,0,1,0,2"/>
+   <wangtile tileid="324" wangid="0,1,0,1,0,1,0,2"/>
+   <wangtile tileid="362" wangid="0,2,0,1,0,2,0,2"/>
+   <wangtile tileid="363" wangid="0,2,0,2,0,1,0,2"/>
+   <wangtile tileid="402" wangid="0,1,0,2,0,2,0,2"/>
+   <wangtile tileid="403" wangid="0,2,0,2,0,2,0,1"/>
+  </wangset>
+  <wangset name="Dense forest" type="corner" tile="-1">
+   <wangcolor name="edges" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="middle" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="645" wangid="0,1,0,2,0,1,0,1"/>
+   <wangtile tileid="646" wangid="0,1,0,1,0,2,0,1"/>
+   <wangtile tileid="685" wangid="0,2,0,1,0,1,0,1"/>
+   <wangtile tileid="686" wangid="0,1,0,1,0,1,0,2"/>
+  </wangset>
+ </wangsets>
+</tileset>
